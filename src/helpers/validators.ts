8c67f07aaee0eb6926e97248446ed651683a8a85
pablos91 <pawel@maple.com.pl>
2019-04-06 19:23:49 +0200

    minor fix to get actual language :)

--- src/helpers/validators.ts
+++ src/helpers/validators.ts
@@ -2,5 +2,5 @@ import * as validator from 'validator';
 import { createRule } from "react-use-validator";
 import i18n from "../../i18n";
 
-export const isUrl = createRule("url", value => validator.isURL(value), i18n.t("url_invalid"));
-export const required = createRule("required", value => !validator.isEmpty(value), i18n.t("field_required"));
+export const isUrl = createRule("url", value => validator.isURL(value), () => i18n.t("url_invalid"));
+export const required = createRule("required", value => !validator.isEmpty(value), () => i18n.t("field_required"));
