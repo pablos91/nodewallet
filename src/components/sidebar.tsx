79eebdfed98756c24187eb3b39808941a93b02d1
pablos91 <pawel@maple.com.pl>
2019-04-06 22:11:45 +0200

    added fs operations but not updating

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -13,18 +13,20 @@ export interface SideBarProps {
 
 const SideBar = (props: SideBarProps) => {
     const { t, i18n } = useTranslation();
-    
+
     return (
         <GlobalContext.Consumer>
-            {({toggleNewNodeModal}) => (
+            {({ toggleNewNodeModal, nodes }) => (
                 <div className="bg-light border-right" id="sidebar-wrapper">
                     <div className="sidebar-heading">Fullnode UI <small>v{packageJson.version}</small> </div>
                     <div className="list-group list-group-flush">
                         {/* this one repeats */}
-                        <NavLink to="/index" className="d-flex list-group-item flex-row align-items-center list-group-item-action">
-                            <FontAwesomeIcon icon={faBitcoin} size="2x" />
-                            <span className="ml-2">Bitcoin</span>
-                        </NavLink>
+                        {nodes.map((elem, index) => (
+                            <NavLink to="/index" className="d-flex list-group-item flex-row align-items-center list-group-item-action">
+                                <FontAwesomeIcon icon={faBitcoin} size="2x" />
+                                <span className="ml-2">Bitcoin</span>
+                            </NavLink>
+                        ))}
                         <a href="javascript:void(0)" onClick={toggleNewNodeModal} className="d-flex list-group-item flex-column align-items-center list-group-item-action">
                             <FontAwesomeIcon icon={faPlusCircle} size="1x" />
                             <span className="">Add another</span>
--- src/contexts/global.tsx
+++ src/contexts/global.tsx
@@ -1,15 +1,44 @@
 import * as React from 'react';
 // every property of this class will be available between react routing navigation (usage: store.property). Refreshing page clears global store so if you want to keep something (like auth) in session use LocalStorage or SessionStorage or Cookies.
 
+const electron = require('electron');
+const path = require('path');
+const fs = require('fs');
+const sha = require("crypto-js/sha256");
+
 export const GlobalContext = React.createContext({
     isNewNodeModalOpen: false,
+    nodes: [],
     toggleNewNodeModal: () => { },
 });
 
+const configPath = path.join((electron.app || electron.remote.app).getPath('userData'), 'config.json');
+
+const defaultConfig = {
+    nodes: [],
+    language: "en"
+};
+
 export const GlobalProvider = ({ children }) => {
+    function readConfigFromDisk() {
+        var config = {...defaultConfig};
+
+        fs.exists(configPath, (exists) => {
+            if (exists) {
+                config = JSON.parse(fs.readFileSync(configPath));
+                console.log(config);
+            } else {
+                fs.writeFileSync(configPath, JSON.stringify(defaultConfig));
+            }
+        });
+
+        return config;
+    }
+    
     const [global, setGlobal] = React.useState({ isNewNodeModalOpen: false });
     const value = {
         isNewNodeModalOpen: global.isNewNodeModalOpen,
+        nodes: readConfigFromDisk().nodes,
         toggleNewNodeModal: () => setGlobal({ isNewNodeModalOpen: !global.isNewNodeModalOpen }),
     };
     return (<GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>);
