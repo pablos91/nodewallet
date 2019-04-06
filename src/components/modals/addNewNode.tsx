4ce17570967a2f4fb083c35faf30fcc3f08d1313
pablos91 <pawel@maple.com.pl>
2019-04-06 14:10:02 +0200

    fun with hooks

--- src/components/modals/addNewNode.tsx
+++ src/components/modals/addNewNode.tsx
@@ -9,10 +9,28 @@ const AddNewModal = () => {
     const {t} = useTranslation();
     const [state, setState] = React.useState<FullNode>({});
 
+    React.useEffect(() => {
+        // component did mount or did update
+        console.log(state.name);
+
+        // cleanup function
+        return () => {
+            console.log('cleanup');
+            //setState({});
+        };
+    });
+
+    const tryAddNewNode = () => {
+
+    }
+    const doCleanup = () => {
+        setState({});
+    }
+
     return (
         <GlobalContext.Consumer>
             {({isNewNodeModalOpen, toggleNewNodeModal}) => (
-                <Modal isOpen={isNewNodeModalOpen} centered>
+                <Modal isOpen={isNewNodeModalOpen} onClosed={doCleanup} centered unmountOnClose={true}>
                     <ModalHeader>{t("add_new_node")}</ModalHeader>
                     <ModalBody>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import { BrowserRouter, RouteProps, RouteComponentProps } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 interface SettingsRouteProps {
   id: string;
 }
 
 const Settings = ({location, match}: RouteComponentProps<SettingsRouteProps>) => {
+  const {t} = useTranslation();
   return (
-    <main>Inna Stronka {location.pathname}</main> 
+    <main>
+      <h2>{t("settings")}</h2>  
+    </main> 
   );
 }
 
