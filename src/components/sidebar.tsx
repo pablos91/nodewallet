cf58dc9400abbf32b3d39ce1da10219eb3abdbbe
pablos91 <pawel@maple.com.pl>
2019-04-13 17:40:11 +0200

    useContext insteadof consumer

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -14,31 +14,29 @@ const packageJson = require('../../package.json');
 
 const SideBar = () => {
     const { t, i18n } = useTranslation();
-    
+    const global = React.useContext(GlobalContext);
+
+
     return (
-        <GlobalContext.Consumer>
-            {({ toggleNewNodeModal, nodes }) => (
-                <div className="bg-light border-right" id="sidebar-wrapper">
-                    <div className="sidebar-heading">Fullnode UI <small>v{packageJson.version}</small> </div>
-                    <div className="list-group list-group-flush">
-                        {/* this one repeats */}
-                        {nodes && nodes.map((elem: FullNodeConfig, index) => (
-                            <SideBarElement key={"sidebar_element_"+index} node={elem} />
-                        ))}
-                        <a href="javascript:void(0)" onClick={toggleNewNodeModal} className="d-flex list-group-item flex-column align-items-center list-group-item-action">
-                            <FontAwesomeIcon icon={faPlusCircle} size="1x" />
-                            <span className="">{t("add_new_node")}</span>
-                        </a>
-                    </div>
-                    <div className="list-group list-group-flush list-group-bottom">
-                        <NavLink to="/settings" className="d-flex list-group-item flex-row align-items-center list-group-item-action">
-                            <FontAwesomeIcon icon={faCog} size="1x" />
-                            <span className="ml-2">{t("settings")}</span>
-                        </NavLink>
-                    </div>
-                </div>
-            )}
-        </GlobalContext.Consumer>
+        <div className="bg-light border-right" id="sidebar-wrapper">
+            <div className="sidebar-heading">Fullnode UI <small>v{packageJson.version}</small> </div>
+            <div className="list-group list-group-flush">
+                {/* this one repeats */}
+                {global.nodes && global.nodes.map((elem: FullNodeConfig, index) => (
+                    <SideBarElement key={"sidebar_element_" + index} node={elem} />
+                ))}
+                <a href="javascript:void(0)" onClick={global.toggleNewNodeModal} className="d-flex list-group-item flex-column align-items-center list-group-item-action">
+                    <FontAwesomeIcon icon={faPlusCircle} size="1x" />
+                    <span className="">{t("add_new_node")}</span>
+                </a>
+            </div>
+            <div className="list-group list-group-flush list-group-bottom">
+                <NavLink to="/settings" className="d-flex list-group-item flex-row align-items-center list-group-item-action">
+                    <FontAwesomeIcon icon={faCog} size="1x" />
+                    <span className="ml-2">{t("settings")}</span>
+                </NavLink>
+            </div>
+        </div>
     );
 }
 
