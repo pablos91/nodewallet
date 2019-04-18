525bbc724a73bbfceda8af8e85a849db684f525b
pablos91 <pawel@maple.com.pl>
2019-04-18 19:39:28 +0200

    added litecoin 0.16 implementation 0.17rc1 gonna make some changes to implementation

---
+++ src/models/nodes/litecoin.ts
@@ -0,0 +1,121 @@
+import { RPCRequest } from "../rpcrequest";
+import { FullNode } from "./node";
+import { FullNodeConfig } from "../fullNodeConfig";
+import Axios, { AxiosRequestConfig, AxiosResponse, AxiosPromise, AxiosError } from "axios";
+import { RPCResponse } from "../rpcresponse";
+import _ = require("lodash");
+import { SendToAddressForm } from "../sendToAddressForm";
+import { Transaction } from "../transaction";
+
+export class Litecoin implements FullNode {
+    symbol: string = "LTC";
+
+    constructor(node: FullNodeConfig) {
+        this.config = {
+            baseURL: node.url,
+            auth: {
+                username: node.rpcuser,
+                password: node.rpcpassword
+            }
+        };
+    }
+
+    private config: AxiosRequestConfig;
+
+    getBalance = () => {
+        return new Promise<number>((resolve, reject) => {
+            Axios.post('/', new RPCRequest("getbalance", [], 1), this.config).then((resp: AxiosResponse<RPCResponse>) => {
+                resolve(resp.data.result);
+            }).catch((error: AxiosError) => {
+                reject(error.response.data.error);
+            })
+        })
+    }
+
+    getAddresses = (label: string = "") => {
+        return new Promise<string[]>((resolve, reject) => {
+            Axios.post('/', new RPCRequest("getaddressesbyaccount", [label], 1), this.config).then((resp: AxiosResponse<RPCResponse>) => {
+                let addresses: string[] = [];
+                _.forOwn(resp.data.result, (v, k) => addresses.push(k));
+                resolve(addresses);
+            }).catch((error: AxiosError) => {
+                reject(error.response.data.error);
+            })
+        })
+    };
+
+    getNewAddress = (label: string = "") => {
+        return new Promise<string>((resolve, reject) => {
+            Axios.post('/', new RPCRequest("getnewaddress", [label], 1), this.config).then((resp: AxiosResponse<RPCResponse>) => {
+                resolve(resp.data.result);
+            }).catch((error: AxiosError) => {
+                reject(error.response.data.error);
+            })
+        })
+    };
+
+    getLabels = () => {
+        return new Promise<string[]>((resolve, reject) => {
+            Axios.post('/', new RPCRequest("listaccounts", [], 1), this.config).then((resp: AxiosResponse<RPCResponse>) => {
+                resolve(resp.data.result);
+            }).catch((error: AxiosError) => {
+                reject(error.response.data.error);
+            })
+        })
+    };
+
+    unlockWallet = (passphrase: string) => {
+        return new Promise<boolean>((resolve, reject) => {
+            Axios.post('/', new RPCRequest("walletpassphrase", [passphrase, 60], 1), this.config)
+                .then((resp: AxiosResponse<RPCResponse>) => {
+                    resolve(true);
+                })
+                .catch((error: AxiosError) => {
+                    if (error.response.data.error.code == -15)
+                        resolve(true);
+                    else
+                        reject(false);
+                })
+        })
+    };
+
+    sendToAddress = (payload: SendToAddressForm) => {
+        return new Promise<string>((resolve, reject) => {
+            Axios.post('/', new RPCRequest("sendtoaddress", [payload.address, payload.amount, payload.comment, "", true], 1), this.config)
+                .then((resp: AxiosResponse<RPCResponse>) => {
+                    resolve(resp.data.result); // thats txid in string
+                })
+                .catch((error: AxiosError) => {
+                    reject(error.response.data.error.message);
+                })
+        })
+    }
+
+    getAllTransactions = () => {
+        return new Promise<Transaction[]>((resolve, reject) => {
+            Axios.post('/', new RPCRequest("listtransactions", [], 1), this.config)
+                .then((resp: AxiosResponse<RPCResponse>) => {
+                    let transactions = [] as Transaction[];
+
+                    if(resp.data.result) {
+                        resp.data.result.map((elem) => {
+                            transactions.splice(0,0,{
+                                amount: elem.amount,
+                                received: elem.category == "receive",
+                                confirmations: elem.confirmations,
+                                id: elem.txid,
+                                date: elem.time,
+                                dateReceived: elem.timereceived,
+                                address: elem.address
+                            });
+                        })
+                    }
+
+                    resolve(transactions);
+                })
+                .catch((error: AxiosError) => {
+                    reject(error.response.data.error.message);
+                })
+        })
+    }
+}
--- src/models/nodes/noderesolver.ts
+++ src/models/nodes/noderesolver.ts
@@ -3,12 +3,15 @@ import { AxiosRequestConfig } from "axios";
 import { FullNode } from "./node";
 import { Bitcoin } from "./bitcoin";
 import { RPCResponse } from "../rpcresponse";
+import { Litecoin } from "./litecoin";
 
 export const NodeResolver = (node: FullNodeConfig) : FullNode => {
 
     switch (node.type) {
         case "bitcoin":
             return new Bitcoin(node);
+        case "litecoin":
+            return new Litecoin(node);
         default:
             return void (0);
     }
