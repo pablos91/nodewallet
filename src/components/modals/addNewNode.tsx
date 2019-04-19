dae23ee40de400f7a041d2130ee71176cf4ca3d2
pablos91 <pawel@maple.com.pl>
2019-04-19 19:20:48 +0200

    switched to useObservable

--- src/components/modals/addNewNode.tsx
+++ src/components/modals/addNewNode.tsx
@@ -11,17 +11,20 @@ import config from '../../helpers/config';
 import { Redirect } from 'react-router';
 import { NodeResolver } from '../../models/nodes/noderesolver';
 import BlockUi from 'react-block-ui';
+import { useObservable, observer } from 'mobx-react-lite';
 
-const AddNewModal = () => {
-    const defaults: FullNodeConfig = {
+const AddNewModal = observer(() => {
+
+    const state = useObservable<FullNodeConfig>({
         name: '',
         type: 'bitcoin',
         url: '',
         rpcpassword: '',
         rpcuser: ''
-    };
+    })
+
     const { t } = useTranslation();
-    const [state, setState] = React.useState<FullNodeConfig>(defaults);
+    // const [state, setState] = React.useState<FullNodeConfig>(defaults);
     const [loading, setLoading] = React.useState(false);
 
     const [messages, validate] = useValidator({
@@ -64,33 +67,33 @@ const AddNewModal = () => {
                     <Form onSubmit={tryAddNewNode}>
                         <FormGroup>
                             <Label>{t("nodename")}</Label>
-                            <Input invalid={messages.name} value={state.name} onChange={(e) => setState({ ...state, name: e.target.value })} />
+                            <Input invalid={messages.name} value={state.name} onChange={(e) => state.name = e.target.value} />
                             <FormFeedback>{messages.name}</FormFeedback>
                         </FormGroup>
                         <FormGroup>
                             <Label>{t("nodetype")}</Label>
-                            <Input onChange={(e) => setState({ ...state, type: e.target.value })} type="select">
+                            <Input onChange={(e) => state.type = e.target.value} type="select">
                                 <option value="bitcoin">Bitcoin</option>
                                 <option value="litecoin">Litecoin</option>
                             </Input>
                         </FormGroup>
                         <FormGroup>
                             <Label>{t("url")}</Label>
-                            <Input invalid={messages.url} value={state.url} onChange={(e) => setState({ ...state, url: e.target.value })} />
+                            <Input invalid={messages.url} value={state.url} onChange={(e) => state.url = e.target.value} />
                             <FormFeedback>{messages.url}</FormFeedback>
                         </FormGroup>
                         <div className="row">
                             <div className="col">
                                 <FormGroup>
                                     <Label>{t("rpcuser")}</Label>
-                                    <Input invalid={messages.rpcuser} value={state.rpcuser} onChange={(e) => setState({ ...state, rpcuser: e.target.value })} />
+                                    <Input invalid={messages.rpcuser} value={state.rpcuser} onChange={(e) => state.rpcuser = e.target.value} />
                                     <FormFeedback>{messages.rpcuser}</FormFeedback>
                                 </FormGroup>
                             </div>
                             <div className="col">
                                 <FormGroup>
                                     <Label>{t("rpcpass")}</Label>
-                                    <Input type="password" invalid={messages.rpcpassword} value={state.rpcpassword} onChange={(e) => setState({ ...state, rpcpassword: e.target.value })} />
+                                    <Input type="password" invalid={messages.rpcpassword} value={state.rpcpassword} onChange={(e) => state.rpcpassword = e.target.value} />
                                     <FormFeedback>{messages.rpcpassword}</FormFeedback>
                                 </FormGroup>
                             </div>
@@ -106,6 +109,6 @@ const AddNewModal = () => {
             </ModalFooter>
         </Modal>
     );
-}
+})
 
 export default AddNewModal;
